20a1d04fd1cdc2b06444a5ecae13c57c66e4d5d7
CrisSaavedra <crisisaave@gmail.com>
2023-02-22 17:42:44 -0300

    combine done

--- Moviet/src/api/getMovies.js
+++ Moviet/src/api/getMovies.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 const URL = 'https://api.themoviedb.org/3/'
 const KEY = '?api_key=b810f50a3a5c617eb3f42a2c5bc7a4c4'
 const IMG_PATH = 'https://image.tmdb.org/t/p/original'
-const DISCOVER = '&page='
+const PAGE = '&page='
 const SEARCH = '&query='
 
 //https://api.themoviedb.org/3/discover/movie?api_key=b810f50a3a5c617eb3f42a2c5bc7a4c4&page=1 discover //movie
@@ -12,29 +12,30 @@ const SEARCH = '&query='
 
 
 
-export const getMovies = async (type_fetch, search) => {
+export const getMovies = async (type_fetch, number, name = '') => {
   //type_fetch = discover / search
-  const url = type_fetch === 'discover' ? URL + type_fetch + '/movie' + KEY + DISCOVER + search : URL + type_fetch + '/movie' + KEY + SEARCH + search
- 
-  const { results } = await fetch(url)
+  const url = type_fetch === 'discover' ?
+  URL + type_fetch + '/movie' + KEY + PAGE + number : URL + type_fetch + '/movie' + KEY + SEARCH + name + PAGE + number 
+
+  const { results, total_pages } = await fetch(url)
     .then(response => response.json())
     .then(data => data);
 
 
 
   const movies = results.map(res => {
-   if( res.poster_path === null){
+    if (res.poster_path === null) {
       return null;
-   }
+    }
     return ({
       id: res.id,
       title: res.original_title,
       img: IMG_PATH + res.poster_path,
     })
-    
+
   })
 
-  return movies
+  return { movies, total_pages }
 
 }
 
